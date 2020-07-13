49b97d51580d295ca8648755f688943229058a42
Dương Tiểu Đồng <duongtieudongminecraft@gmail.com>
2020-07-13 21:44:07 +0700

    Added user input for encoding/decoding.

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ def encode(string: str):
 
 def decode(string: str):
     return codecs.decode(string, "rot_13")
+
+
+if __name__ == "__main__":
+    mode = str(input("What are we doing today sire? (e)ncode or (d)ecode? "))
+    if mode == "e":
+        text = str(input("Enter text to encode: "))
+        print("ROT13 Encoded text: {0}".format(encode(text)))
+    elif mode == "d":
+        text = str(input("Enter text to decode: "))
+        print("ROT13 Decoded text: {0}".format(decode(text)))
+    else:
+        print("Unrecognized mode, exiting.")
+        exit(0)
