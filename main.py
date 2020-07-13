8aadbd75496038aaea8b615213be55a996338e3b
Dương Tiểu Đồng <duongtieudongminecraft@gmail.com>
2020-07-13 20:43:20 +0700

    Basic implementation with codecs library.

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Simple ROT13 encoding/decoding with Python
+"""
+
+import codecs
+
+def encode(string: str):
+    return codecs.encode(string, "rot_13")
+
+def decode(string: str):
+    return codecs.decode(string, "rot_13")
